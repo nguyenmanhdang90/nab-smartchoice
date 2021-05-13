4b5bed7799f39b22b412298bcfde0fb62e52f83f
Dang Nguyen <nguyenmanhdang90@users.noreply.github.com>
2021-05-13 23:26:19 +0700

    init project
    create smartchoice-fapi including:
    
      - smartchocie-api
      - smartchoice-db
    
    create smartchoice-common

---
+++ README.md
@@ -0,0 +1,15 @@
+# Smart Choice
+
+
+## Technical Specs:
+
+IDE:
+ 1. [IntelliJ community](https://www.jetbrains.com/idea/)
+ 
+Technical stack
+ 
+  
+## Start development process:
+
+
+## How to run
---
+++ smartchoice-fapi/smartchoice-api/src/main/java/com/nab/smartchoice/api/SmartChoiceApiApplication.java
@@ -0,0 +1,12 @@
+package com.nab.smartchoice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartChoiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartChoiceApiApplication.class, args);
+    }
+}
